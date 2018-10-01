6e760716995eecf980fc438f0edfefe1c97de763
jonasschjerlund <jonassc@ruc.dk>
2018-10-01 12:04:48 +0200

    Fixed shit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TowerDefence
+# Tower_Defense_Game
+This is a game XD
+Indeed, it is. 
---
+++ Tower Defense Game/Assets/Scripts/EnemySpecs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpecs : MonoBehaviour {
+
+    public int health = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Damaged(int amount) {
+        health -= amount;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
