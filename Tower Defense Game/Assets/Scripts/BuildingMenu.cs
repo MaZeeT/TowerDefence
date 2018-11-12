6862d177c3fa048851df2b6bb7744e0bb5bf1f85
jonasschjerlund <jonassc@ruc.dk>
2018-11-12 14:20:26 +0100

    12/11 Lasserpointer XD

--- Tower Defense Game/Assets/Scripts/BuildingMenu.cs
+++ Tower Defense Game/Assets/Scripts/BuildingMenu.cs
@@ -5,21 +5,22 @@ using Valve.VR;
 public class BuildingMenu: MonoBehaviour
 {
     SteamVR_TrackedObject obj; // finding the controller
-
+    // Setup functioner
     public GameObject buttonHolder; //empty object that contains the buttons
-
-    public bool buttonEnabled; // saying whether or the empty object is enabled
-
-    public bool holding;
-    public bool holdingFoundation;
-    public bool holdingBall;
-
+    public GameObject builderPointer;
     public GameObject TowerFoundation;
     public GameObject BuildingBall;
     public GameObject Hand;
 
+    public bool buttonEnabled; // saying whether or the empty object is enabled
 
- 
+    // Public booleans, need to be accesed in other scripts
+    public bool holding;
+    public bool holdingFoundation;
+    public bool holdingBall;
+   
+
+    
     void Start()
     {
         obj = GetComponent<SteamVR_TrackedObject>();
@@ -61,10 +62,17 @@ public class BuildingMenu: MonoBehaviour
         var device = SteamVR_Controller.Input(3);
 
         if (holdingFoundation) {
-            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                builderPointer.SetActive(true);
+            }
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
 
                 Vector3 vec = new Vector3(Hand.transform.position.x, 0.01f, Hand.transform.position.z);
                 Instantiate(TowerFoundation, vec, Quaternion.identity);
+
+                builderPointer.SetActive(false);
                 holdingFoundation = false;
                 holding = false;
             }
@@ -73,10 +81,18 @@ public class BuildingMenu: MonoBehaviour
         if (holdingBall) {
             if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
 
+                builderPointer.SetActive(true);
+            }
+
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+
                 Vector3 vec = new Vector3(Hand.transform.position.x, 0.01f, Hand.transform.position.z);
                 Instantiate(BuildingBall, vec, Quaternion.identity);
+
+                builderPointer.SetActive(false);
                 holdingBall = false;
                 holding = false;
+
             }
         }
     } // placeObjecft end
