fb567bffc743e5f164bc7619425aa77ecd7089c0
jonasschjerlund <jonassc@ruc.dk>
2018-12-10 15:15:54 +0100

    Added upgrade types

--- Tower Defense Game/Assets/Scripts/Hand Menu/BuildingMenu.cs
+++ Tower Defense Game/Assets/Scripts/Hand Menu/BuildingMenu.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using Valve.VR;
 
-public class BuildingMenu: MonoBehaviour
+public class BuildingMenu : MonoBehaviour
 {
 
     Vector3 BenjaminPoints = new Vector3();
     SteamVR_TrackedObject obj; // finding the controller
     [Header("Setup")]
+    public int itemID;
     public GameObject buttonHolder; //empty object that contains the buttons
     public GameObject Hand;
     public GameObject TowerFoundation;
@@ -20,9 +21,9 @@ public class BuildingMenu: MonoBehaviour
     public bool holding;
     public bool holdingFoundation;
     public bool holdingBall;
-   
 
-    
+
+
     void Start()
     {
         obj = GetComponent<SteamVR_TrackedObject>();
@@ -31,17 +32,19 @@ public class BuildingMenu: MonoBehaviour
         holdingFoundation = false;
         holdingBall = false;
         holding = false;
-       
+
 
     } // Start end
 
     void Update()
     {
         MenuOpen();
+        CheckItemID();
         PlaceObject();
     } // Update end
 
-    void MenuOpen () {
+    void MenuOpen()
+    {
 
         var device = SteamVR_Controller.Input(4); //you are getting the device and setting it here
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu)) //When you press the button above the Dpad you will do this function
@@ -60,36 +63,54 @@ public class BuildingMenu: MonoBehaviour
         }
     }// MenuOpen end
 
-    void PlaceObject() {
+    void CheckItemID()
+    {
+        var device = SteamVR_Controller.Input(4);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Axis0)) {
+            itemID++;
+            Debug.Log(itemID);
+        } else if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Axis1)) {
+            itemID--;
+            Debug.Log(itemID);
+        }
+    }
+
+    void PlaceObject()
+    {
 
         var device = SteamVR_Controller.Input(3);
-      
 
 
-        if (holdingFoundation) {
+
+        if (holdingFoundation)
+        {
 
             if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
             {
                 Debug.DrawRay(Hand.transform.position, Hand.transform.forward * 50, Color.red);
-                
+
             }
-            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
                 Vector3 vec = new Vector3(Hand.transform.position.x, 0.1f, Hand.transform.position.z);
                 Instantiate(TowerFoundation, vec, Quaternion.identity);
 
                 holdingFoundation = false;
                 holding = false;
             }
-            
+
 
         }
-        if (holdingBall) {
-            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+        if (holdingBall)
+        {
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
                 Debug.DrawRay(Hand.transform.position, Hand.transform.forward * 50, Color.red);
 
             }
 
-            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
                 Vector3 vec = new Vector3(Hand.transform.position.x, Hand.transform.position.y, Hand.transform.position.z);
                 Instantiate(BuildingBall, vec, Hand.transform.rotation);
 
