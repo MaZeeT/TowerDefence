427c9ed934e3b6b8e4803126fe0aaebaf99e1e39
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2018-12-11 11:36:46 +0100

    a few updates

--- Tower Defense Game/Assets/Scripts/BaseSpecs.cs
+++ Tower Defense Game/Assets/Scripts/BaseSpecs.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseSpecs : MonoBehaviour {
-
+    GameObject overlord = GameObject.FindGameObjectWithTag("Overlord");
     bool gameover = false;
 
     [Header("Setup")]
-    public int Health;
+    public int health;
 
     void OnTriggerEnter(Collider Other) {
         if (Other.tag == "Enemy") {
-            Health--;
-            if (Health <= 0) {
+            health--;
+            if (health <= 0) {
                 gameover = true;
                 Destroy(Other.gameObject);
+                overlord.GetComponent<Overlord>().DecreaseMinionCount();
                 Destroy(gameObject);
             }
             else
             {
                 Destroy(Other.gameObject);
+                overlord.GetComponent<Overlord>().DecreaseMinionCount();
             }
         }
     }// OnTriggerEnter end
+
+    public void ReduceHealth(int amount)
+    {
+        health = health - amount;
+    }
 }
