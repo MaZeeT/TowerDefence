ad64caf928dcd83ea670d8817a563ec799e5654a
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2018-12-11 11:56:06 +0100

    Update BaseSpecs.cs

--- Tower Defense Game/Assets/Scripts/BaseSpecs.cs
+++ Tower Defense Game/Assets/Scripts/BaseSpecs.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseSpecs : MonoBehaviour {
-    GameObject overlord = GameObject.FindGameObjectWithTag("Overlord");
+    GameObject overlord;
     bool gameover = false;
 
     [Header("Setup")]
     public int health;
 
+    private void Start()
+    {
+         overlord = GameObject.FindGameObjectWithTag("Overlord");
+    }
     void OnTriggerEnter(Collider Other) {
         if (Other.tag == "Enemy") {
             health--;
