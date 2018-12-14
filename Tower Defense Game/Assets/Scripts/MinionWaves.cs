f107e29d1e0177e834250a5ca3b30ad964002f11
jonasschjerlund <jonassc@ruc.dk>
2018-12-14 11:22:51 +0100

    Update MinionWaves.cs

--- Tower Defense Game/Assets/Scripts/MinionWaves.cs
+++ Tower Defense Game/Assets/Scripts/MinionWaves.cs
@@ -29,7 +29,7 @@ public class MinionWaves : MonoBehaviour {
         this.path = path;
 
         minionsToSpawn = waveSize;
-            isSpawning = true;                            
+        isSpawning = true;                            
     }
     private void Update()
     {
