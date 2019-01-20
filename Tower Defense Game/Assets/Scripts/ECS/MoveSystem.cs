0cfff13445476c415574968d54702933999602d3
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-01-20 16:13:22 +0100

    added move system + component

---
+++ Tower Defense Game/Assets/Scripts/ECS/MoveSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class MoveSystem : ComponentSystem {
+
+    struct Components
+    {
+        public Transform transform;
+        public MoveComponent moveComponent;
+        public PathFinding pathFinding;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach (var entities in GetEntities<Components>())
+        {
+
+        }
+        throw new System.NotImplementedException();
+    }
+
+}
