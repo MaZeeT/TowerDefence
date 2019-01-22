9befe742158729b5c99884492331c79638c26450
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-01-22 22:09:17 +0100

    Update to ECS

--- Tower Defense Game/Assets/Scripts/ECS/MoveComponent.cs
+++ Tower Defense Game/Assets/Scripts/ECS/MoveComponent.cs
@@ -1,12 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using Unity.Entities;
-
 
 public class MoveComponent : MonoBehaviour
 {
     public float speed;
 }
-
-//public class MoveSpeedComponent : ComponentDataWrapper<MoveComponent> { }
--- Tower Defense Game/Assets/Scripts/ECS/MoveSystem.cs
+++ Tower Defense Game/Assets/Scripts/ECS/MoveSystem.cs
@@ -8,61 +8,18 @@ public class MoveSystem : ComponentSystem {
     struct Components
     {
         public Transform transform;
-        public MoveComponent moveComponent;
-        public MoveToTargetComponent target;
-        public Rigidbody rigidbody;
+        public MoveComponent move;
+        public DestinationComponent destination;
     }
 
     protected override void OnUpdate()
     {
         foreach(var entities in GetEntities<Components>())
         {
-            Vector3 direction = entities.transform.position - entities.target.transform.position;
-            //float moveSpeed = entities.moveComponent.speed;
-            float moveSpeed = 2;
-            
+            Vector3 direction = entities.transform.position - entities.destination.transform.position;
+            float moveSpeed = entities.move.speed;
+
             entities.transform.Translate(direction.normalized * moveSpeed * Time.deltaTime, Space.World);
         }
     }
-
-
-    /*
-    protected override void OnUpdate()
-    {
-        float deltaTime = Time.deltaTime;
-
-        foreach (var entities in GetEntities<Components>())
-        {
-            Vector3 moveVector;
-            GameObject targetObject;
-
-            float speed = entities.moveComponent.speed;
-            targetObject = entities.target.target; 
-            moveVector = targetObject.transform.position - entities.transform.position;
-            var movePosition = entities.rigidbody.position + moveVector.normalized * speed * deltaTime;
-
-            entities.rigidbody.MovePosition(movePosition);
-
-            //entities.transform.Translate(direction.normalized * 1 * Time.deltaTime, Space.World);
-        }
-    }
-
-     Vector3 position;
-            position = entities.transform.position;
-
-            Quaternion rotation;
-            rotation = entities.transform.rotation;
-
-            float speed;
-            speed = entities.moveComponent.speed;
-
-            Vector3 moveVector;
-            moveVector = entities.target.transform.position - entities.transform.position;
-
-            var movePosition = entities.rigidbody.position + moveVector.normalized * speed * Time.deltaTime;
-            entities.rigidbody.MovePosition(movePosition);
-            //entities.transform.SetPositionAndRotation(position + moveVector, rotation);
-
-
-    */
 }
